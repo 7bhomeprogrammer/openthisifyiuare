2cf22ea41b1bb047e330c7fc615b6c5fdcde8033
7bhomeprogrammer <richazatbek999@gmail.com>
2025-01-19 17:07:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Список допустимых имен (добавь своё имя в список)
-const allowedNames = ["Dilfuza", "dilfuza", "DILFUZA", "Дилфуза", "дилфуза", "ДИЛФУЗА", "Azatbek,","Адема","adema","Adema"]
 
-// Событие при нажатии кнопки
-document.getElementById("submit-btn").addEventListener("click", () => {
-    const inputName = document.getElementById("name").value.trim(); // Убираем пробелы
-    if (allowedNames.includes(inputName)) {
-        // Если имя допустимо
-        document.getElementById("auth-container").style.display = "none";
-        document.getElementById("message-container").style.display = "block";
-    } else {
-        // Если имя неверное
-        document.getElementById("error-message").style.display = "block";
-    }
-});
